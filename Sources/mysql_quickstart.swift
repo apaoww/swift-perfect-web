74e686e13548cc3bbf9b8a3072e65fa14e833220
Safi'ee Madian <apa.oww@gmail.com>
2017-01-18 10:52:34 +0800

    Update mysql_quickstart.swift

--- Sources/mysql_quickstart.swift
+++ Sources/mysql_quickstart.swift
@@ -3,9 +3,9 @@ import MySQL
 import PerfectHTTP
 
 let testHost = "127.0.0.1"
-let testUser = "root"
+let testUser = "username"
 // PLEASE change to whatever your actual password is before running these tests
-let testPassword = "878220"
+let testPassword = "password"
 let testSchema = "test"
 
 let dataMysql = MySQL()
